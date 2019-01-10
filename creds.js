c510f5c68565241f697c0d46edc151b9389b969a
masterbarodckevil <46573566+masterbarodckevil@users.noreply.github.com>
2019-01-11 00:05:38 +0530

    Update creds.js

--- creds.js
+++ creds.js
@@ -1,4 +1,5 @@
 module.exports = {
-    username: '<GITHUB_USERNAME>',
-    password: '<GITHUB_PASSWORD>'
+    const USERNAME_SELECTOR = '#user\[login\]';
+    const PASSWORD_SELECTOR = '#user\[password\]';
+    const BUTTON_SELECTOR = 'button.btn-mktg:nth-child(10)';
 }
