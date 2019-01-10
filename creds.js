a00becf3ff85d7c54c0b36cf5ca7711f63f012f9
masterbarodckevil <46573566+masterbarodckevil@users.noreply.github.com>
2019-01-11 00:12:47 +0530

    Update creds.js

--- creds.js
+++ creds.js
@@ -3,3 +3,12 @@ module.exports = {
     const PASSWORD_SELECTOR = '#user\[password\]';
     const BUTTON_SELECTOR = 'button.btn-mktg:nth-child(10)';
 }
+await page.click(USERNAME_SELECTOR);
+await page.keyboard.type(CREDS.username);
+
+await page.click(PASSWORD_SELECTOR);
+await page.keyboard.type(CREDS.password);
+
+await page.click(BUTTON_SELECTOR);
+
+await page.waitForNavigation();
